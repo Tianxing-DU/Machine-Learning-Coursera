29ed13d09be8ef94c819c57a33a32168f24ac93c
Tianxing-DU <72757576+Tianxing-DU@users.noreply.github.com>
2021-11-18 19:08:07 +0100

    Update and rename computeCostMulti.m to 2_computeCostMulti.m

--- 2_computeCostMulti.m
+++ 2_computeCostMulti.m
@@ -7,12 +7,13 @@ function J = computeCostMulti(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-J = (0.5/m) * (X*theta - y)' * (X*theta - y);
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+J = 0;
+J = (0.5/m) * (X*theta - y)' * (X*theta - y);
 % (这三排也会是可以的)predictions = X*theta;
 % sqrErrors = (predictions -y).^2;
 % J=1/(2*m) * sum(sqrErrors);
