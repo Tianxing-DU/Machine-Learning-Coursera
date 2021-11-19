69589b9a461d043ad1a435569142b4527d595f8a
Tianxing-DU <72757576+Tianxing-DU@users.noreply.github.com>
2021-11-19 14:27:01 +0100

    Update and rename costFunction.m to 3_costFunction.m

--- 3_costFunction.m
+++ 3_costFunction.m
@@ -9,7 +9,7 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
+grad = zeros(size(theta));% grad就是gradient，delta（J)
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
