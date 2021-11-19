1affece4c3b3e4d887552ea365cff8da10e72286
Tianxing-DU <72757576+Tianxing-DU@users.noreply.github.com>
2021-11-19 14:10:33 +0100

    Add files via upload
    here is programming exercise 3 for logistic regression

---
+++ costFunction.m
@@ -0,0 +1,37 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+
+h_x = sigmoid(X*theta);
+J = ((-y)'*log(h_x)-(1-y)'*log(1-h_x))/m;
+% 这里也可以用sum，那么就需要transpose log（h_x),而不是y。
+% transpose y的好处是把matrix变成了1X1， 而不是100X100，相当于直接求sum
+% 换句话说，如果遇到求sum的，试一试能不能变成1X1的matrix
+
+grad = 1/m * X'*(h_x - y);
+
+
+
+
+
+% =============================================================
+
+end
---
+++ plotData.m
@@ -0,0 +1,33 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+% Using for loop and if condition, however legend cannot be plotted properly. 
+% Considering I will not use it for my research visualisation, so here is just okay.
+% 我的感悟是，必须要plot 两个data两个独立的命令，legend才会分别赋值
+
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 5);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 5);
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
